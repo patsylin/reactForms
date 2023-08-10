ad59a7f51984835c1de488dce731f8f930edb47e
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2023-08-10 12:52:40 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Authenticate from './components/Authenticate'
+import SignUpForm from './components/SignUpForm'
+import { useState } from "react";
+import './App.css'
+
+
+export default function App() {
+const [token, setToken] = useState(null);
+
+
+return (
+<>
+<SignUpForm token={token} setToken={setToken} />
+<Authenticate token={token} setToken={setToken} />
+</>
+);
+}
